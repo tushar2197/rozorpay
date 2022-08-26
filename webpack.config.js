e55e4bc0a53d8a044ea8a369272a11a004950ca3
savaliya tushar <40295103+tushar2197@users.noreply.github.com>
2022-08-26 11:26:39 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.join(__dirname, "src", "index"),
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "build")
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
